82031dd4d63fd34e7c601765ab103d2657a2d6e7
Nurul2022 <nurulbdpersonal@gmail.com>
2022-02-03 17:57:30 +0600

    final

--- test.js
+++ test.js
@@ -1,7 +1,36 @@
 //Quiz No 08 solution
 
+/*
 var a = 2;
 var b = 3;
 if (a < b) {
     console.log("Hello");
 }
+*/
+
+/*
+function add(a, b){
+  return  a + b;
+}
+console.log(add("adam" + "eve"));
+*/
+
+/*
+var first = "Rafsan";
+var second = "Samira";
+console.log(first, second);
+var temp = first;
+first = second;
+second = temp;
+console.log(first, second);
+*/
+
+var sentence = "I am hardworking. I am determined. I will be a web developer.";
+var count = 0;
+for(var i = 0; i < sentence.length; i++){
+  var letter = sentence[i];
+  if(letter == 'a'){
+    count++;
+  }
+}
+console.log(count)
